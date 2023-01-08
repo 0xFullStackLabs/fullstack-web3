5ece57ba05acaa7ccf4408789ee04c94eb8d0555
Yi Song (Ethan) <ethaninmel@gmail.com>
2023-01-08 15:48:15 +0800

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+  staticImage: true,
+  latex: true,
+  flexsearch: {
+    codeblocks: true
+  },
+  defaultShowCopyCode: true
+})
+
+module.exports = withNextra()
---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Web3全栈开发</span>,
+  project: {
+    link: 'https://github.com/ethaninmel/fullstack-web3',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/ethaninmel/fullstack-web3',
+  footer: {
+    text: 'Web3全栈开发',
+  },
+}
+
+export default config
